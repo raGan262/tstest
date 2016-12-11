64c1ce30b711663ab8711aa69ca175796d1ab7b3
David Molnar <molnardad@gmail.com>
2016-12-11 12:35:12 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        filename: 'dist/bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+}
